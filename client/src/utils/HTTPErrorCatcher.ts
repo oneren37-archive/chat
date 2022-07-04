82bff9bb5f885d72b1b2314720e2ec2420667001
Руденко Михаил <60698077+oneren37@users.noreply.github.com>
2022-07-04 16:48:40 +0300

    Revert "Revert "Авторизация""

---
+++ client/src/utils/HTTPErrorCatcher.ts
@@ -0,0 +1,15 @@
+export async function HTTPErrorCatcher(res: any) {
+    if (!res.ok) {
+        // 400 - ошибки бизнес логики
+        if (res.status === 400) throw new Error(await res.text());
+        throw new Error(
+            JSON.stringify({
+                name: 'Server Error',
+                code: null,
+                subCode: null,
+                message: 'Unknown error',
+            }),
+        );
+    }
+    return res;
+}
