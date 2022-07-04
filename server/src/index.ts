75b8066f7cf5509659ee64ee5d5445259ad2a890
Руденко Михаил <60698077+oneren37@users.noreply.github.com>
2022-07-04 15:51:10 +0300

    Revert "Авторизация"

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,11 @@
-import * as path from 'path';
-import { SignUpEndpoint } from './api/SignUpEndpoint';
-import { SignInEndpoint } from './api/SignInEndpoint';
-import { AuthEndpoint } from './api/AuthEndpoint';
-
-const cookieParser = require('cookie-parser');
-
 const express = require('express');
 
 const PORT = process.env.PORT || 8080;
+
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
-app.use(express.static(path.resolve(`${__dirname}../../../client/build/`)));
-
-app.post('/api/sign-up', SignUpEndpoint);
-app.post('/api/sign-in', SignInEndpoint);
-app.get('/api/is-auth', AuthEndpoint);
-
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(`${__dirname}../../../client/build/index.html`));
+app.get('/', (req, res) => {
+  res.send("IT'S WORKING");
 });
 
 app.listen(PORT, () => console.log('сервер запустился'));
